e11f3e7e932f9006c37c6c6902c986453f645c19
phellman-CUNA <151675643+phellman-CUNA@users.noreply.github.com>
2023-11-29 11:36:11 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ streamlit.dataframe(fruits_to_show)
 # write your own comment - what does this do?
 #streamlit.dataframe(fruityvice_normalized)
 # function
-def get_fruity_vice_data(this_fruit_choice):
+def get_fruityvice_data(this_fruit_choice):
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
