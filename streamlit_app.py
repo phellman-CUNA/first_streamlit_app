9245feeca396ee66bffee44efe54615688703648
phellman-CUNA <151675643+phellman-CUNA@users.noreply.github.com>
2023-11-29 11:32:47 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,8 +34,8 @@ streamlit.dataframe(fruits_to_show)
 # function
 def get_fruity_vice_data(this_fruit_choice):
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    return fruityvice_normalized
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
 
 #new section fruityvice
 streamlit.header("Fruityvice Fruit Advice!")
